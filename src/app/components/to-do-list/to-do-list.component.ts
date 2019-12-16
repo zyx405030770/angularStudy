566e77349ad9660b9a3bf12ba01a2133006edae4
zyx405030770 <zyx405030770@sina.com>
2019-12-16 23:23:21 +0800

    fix:修复了京东部分bug

--- src/app/components/to-do-list/to-do-list.component.ts
+++ src/app/components/to-do-list/to-do-list.component.ts
@@ -15,7 +15,7 @@ import { Title } from '@angular/platform-browser';
 export class ToDoListComponent implements OnInit {
 
   keyWords: any = '';
-  historyList: any = [null]; //怎样在列表初始化为空的时候实现功能？
+  historyList: any = ['']; //怎样在列表初始化为空的时候实现功能？
 
   constructor(public msg: NzMessageService) { }
 
@@ -23,15 +23,19 @@ export class ToDoListComponent implements OnInit {
   }
 
   doSearch() {
-
-    this.historyList.push(this.keyWords);
-    console.log(this.keyWords);
-    this.keyWords = '';
-
+    if (this.historyList.indexOf(this.keyWords) == -1) {
+      this.historyList.push(this.keyWords);
+    } this.keyWords = '';
   }
+
   keyup(e) {
     if (e.key == 'Enter') {
       this.doSearch()
     }
+  }
+
+  deleteHistory(key) {
+    alert(key);
+    this.historyList.splice(key, 1);
   }
 }
